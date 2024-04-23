5f5e287a13ae53451d2e0008a1860e47f46d5a81
Sagiv Abu <sagivabu101@gmail.com>
2024-04-23 22:26:05 +0300

    Ideas for main

--- kerberos/main.py
+++ kerberos/main.py
@@ -3,6 +3,19 @@
 
 def main():
     try:
+        # Upload Authentication Server
+        
+        # Upload 2 Clients
+            # Register both
+            # Ask for symmetric key for server 1
+            # Send symmetric key
+            # Send a message
+            # Down and upload the same client (no need for registratrion)
+        
+        # Upload 2 Message Servers
+            # Register both
+            
+        # Monkeys tests?
         pass
     except Exception as e:
         print(f"ERROR occured while running 'main' function to test all the flow.\t{e}")
